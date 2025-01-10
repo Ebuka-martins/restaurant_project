9e0bfef1f0e198a8db7724f9449837debb009977
Ebuka MartinsEchesi <martin4best@gmail.com>
2025-01-10 18:34:29 +0000

    Update database configuration

--- restaurant_project/settings.py
+++ restaurant_project/settings.py
@@ -65,10 +65,7 @@ WSGI_APPLICATION = 'restaurant_project.wsgi.application'
 
 # Database configuration
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600
-    )
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 # Password validation
